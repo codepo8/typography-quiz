633a069c3209702bda3cecc47cef089040fc7dbf
Chris Heilmann <chris.heilmann@gmail.com>
2019-02-11 18:41:12 +0100

    external css

--- typography-quiz.js
+++ typography-quiz.js
@@ -1,58 +1,17 @@
 const information = {
-	'e1': 'tittle',
-	'e2': 'shoulder',
-	'e3': 'aperture',
-	'e4': 'stem',
-	'e5': 'spur',
-	'e6': 'axis',
-	'e7': 'shoulder',
-	'e8': 'ascender',
-	'e9': 'descender',
-	'e10': 'finial',
-	'e11': 'crossbar',
-	'e12': 'bowl',
-	'e13': 'terminal',
-	'e14': 'terminal',
-	'e15': 'loop',
-	'e16': 'link/neck',
-	'e17': 'ear',
-	'e18': 'tail',
-	'e19': 'crossbar',
-	'e20': 'apex',
-	'e21': 'vertex',
-	'e22': 'overhang',
-	'e23': 'apex',
-	'e24': 'apex',
-	'e25': 'beak',
-	'e26': 'joint',
-	'e27': 'bracket',
-	'e28': 'bracket',
-	'e29': 'crossbar',
-	'e30': 'arc',
-	'e31': 'bilateral serif',
-	'e32': 'bilateral serif',
-	'e33': 'foot',
-	'e34': 'counter (closed)',
-	'e35': 'hairline (stroke)',
-	'e36': 'crotch',
-	'e37': 'leg',
-	'e38': 'arm',
-	'e39': 'bowl',
-	'e40': 'eye',
-	'e41': 'counter (open)',
-	'e42': 'arm',
-	'e43': 'stress',
-	'e44': 'serif',
-	'e45': 'ligature',
-	'e46': 'beak',
-	'e47': 'spine',
-	'e48': 'head serif',
-	'e49': 'head serif'
+	'e1': 'tittle', 'e2': 'shoulder', 'e3': 'aperture', 'e4': 'stem',
+	'e5': 'spur', 'e6': 'axis', 'e7': 'shoulder', 'e8': 'ascender', 'e9': 'descender',
+	'e10': 'finial', 'e11': 'crossbar', 'e12': 'bowl', 'e13': 'terminal', 'e14': 'terminal', 
+	'e15': 'loop', 'e16': 'link/neck', 'e17': 'ear', 'e18': 'tail', 'e19': 'crossbar',
+	'e20': 'apex', 'e21': 'vertex', 'e22': 'overhang', 'e23': 'apex', 'e24': 'apex',
+	'e25': 'beak', 'e26': 'joint', 'e27': 'bracket', 'e28': 'bracket', 'e29': 'crossbar',
+	'e30': 'arc', 'e31': 'bilateral serif', 'e32': 'bilateral serif', 'e33': 'foot','e34': 'counter (closed)',
+	'e35': 'hairline (stroke)', 'e36': 'crotch', 'e37': 'leg', 'e38': 'arm', 'e39': 'bowl',
+	'e40': 'eye', 'e41': 'counter (open)', 'e42': 'arm', 'e43': 'stress', 'e44': 'serif',
+	'e45': 'ligature', 'e46': 'beak', 'e47': 'spine', 'e48': 'head serif', 'e49': 'head serif'
 }
 const createdrop = () => {
-	let dropoptions = new Set(
-			Object.values(information)
-	);
+	let dropoptions = new Set(Object.values(information));
 	let str = `<option>${[...dropoptions].sort().join('</option><option>')}</option>`;
 	document.querySelector('#type-elements').innerHTML = str;
 }
@@ -68,9 +27,9 @@ const setbanner = (msg, x, y) => {
 const setEventHandlers = () => {
 	let elements = document.querySelector('#elements');
 	elements.addEventListener('click', handleselect);
-	elements.addEventListener('touchstart', handleselect);
-}	
-const handleselect = (ev) => {
+}
+	const handleselect = (ev) => {
+	console.log(ev);
 	if (ev.target.nodeName.toLowerCase() === 'path' || 
 				ev.target.nodeName.toLowerCase() === 'polygon' ||
 				ev.target.nodeName.toLowerCase() === 'g' ) {
